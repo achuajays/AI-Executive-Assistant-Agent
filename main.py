7ad3c8ac50401923a5b4da2f0e5517e432ab8d88
Adarsh Ajay <adarshajays139@gmail.com>
2025-05-08 00:26:38 -0700

    main added

--- main.py
+++ main.py
@@ -82,3 +82,7 @@ def delete_todo(todo_id: int, db: Session = Depends(get_db)):
     return None
 
 
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
